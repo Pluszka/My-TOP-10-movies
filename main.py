62b60e75397e5db5f45fa5f51366efb09633135e
Pliszka <karoliniakkjk@gmail.com>
2022-04-26 22:31:44 +0200

    created database

--- main.py
+++ main.py
@@ -7,9 +7,35 @@ from wtforms.validators import DataRequired
 import requests
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///FilmList.db'
+db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), unique=True, nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, unique=True, nullable=True)
+    review = db.Column(db.String(250), unique=True, nullable=True)
+    img_url = db.Column(db.String(250), unique=True, nullable=False)
+
+#db.create_all()
+
+# new = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+# db.session.add(new)
+# db.session.commit()
 
 @app.route("/")
 def home():
